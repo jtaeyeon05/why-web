8253ee9913310d9bdb7dafc293bcf80a82a0fe76
jtaeyeon05 <jtaeyeon05@gmail.com>
2026-03-10 14:26:19 +0900

    test(web): use local mona12 fonts and compose root

--- composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/App.kt
+++ composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/App.kt
@@ -7,10 +7,24 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.FontLoadResult
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.configureWebResources
+import org.jetbrains.compose.resources.preloadFont
+
+import why_web.composeapp.generated.resources.Mona12
+import why_web.composeapp.generated.resources.Mona12_Bold
+import why_web.composeapp.generated.resources.Res
 
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     AppTheme {
