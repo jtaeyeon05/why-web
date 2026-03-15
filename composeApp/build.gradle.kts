a4a078d1b00357a2e017eafef9b4fd6f1576e802
jtaeyeon05 <jtaeyeon05@gmail.com>
2026-03-16 03:24:21 +0900

    feat(web): add moon and stars animation to EarthScreen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     kotlin("plugin.serialization") version "2.3.0"
 }
 
-val version = "0.0.16B"
+val version = "0.0.17B"
 val buildInfoPackage = "io.github.jtaeyeon05.why_web.buildinfo"
 val buildInfoDir = layout.buildDirectory.dir("generated/sources/buildInfo/kotlin")
 
--- composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/ui/screen/EarthScreen.kt
+++ composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/ui/screen/EarthScreen.kt
@@ -1,6 +1,15 @@
 package io.github.jtaeyeon05.why_web.ui.screen
 
+import androidx.compose.animation.core.EaseInOutCubic
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.StartOffset
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,12 +19,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.times
 import androidx.navigation.NavController
 import io.github.jtaeyeon05.why_web.navigation.Screen
 import io.github.jtaeyeon05.why_web.ui.foundation.LocalLayoutConstraints
 import io.github.jtaeyeon05.why_web.ui.foundation.TextSize
 import kotlinx.coroutines.delay
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.random.Random
 
 
 @Composable
@@ -24,24 +39,76 @@ fun BoxScope.EarthScreen(
     screen: Screen.Earth,
 ) {
     LocalLayoutConstraints.current.run {
-        val earth = listOf("🌍", "🌎", "🌏")
-        var earthIndex by remember { mutableStateOf(0) }
-
         val density = LocalDensity.current
         val earthSize = TextSize(
             size = this.screen.base * 0.32f,
             lineHeight = 1f,
             density = density
         )
+        val moonSize = TextSize(
+            size = this.screen.base * 0.12f,
+            lineHeight = 1f,
+            density = density
+        )
+        val starSize = TextSize(
+            size = this.screen.base * 0.02f,
+            lineHeight = 1f,
+            density = density
+        )
+
+        val earth = listOf("🌍", "🌎", "🌏")
+        val moon = listOf("🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘")
+
+        var earthIndex by remember { mutableStateOf(0) }
+        var moonIndex by remember { mutableStateOf(Random.nextInt(moon.size)) }
+        val starPosition by remember(this) {
+            mutableStateOf(
+                List(20) {
+                    this.screen.run {
+                        Pair(
+                            cos(Random.nextFloat() * PI) * 0.45f * width,
+                            cos(Random.nextFloat() * PI) * 0.45f * height
+                        )
+                    }
+                }
+            )
+        }
+        val starSettings = remember {
+            List(20) {
+                this.screen.run {
+                    val duration = Random.nextInt(500, 2000)
+                    val offset = Random.nextInt(0, duration)
+                    Pair(duration, offset)
+                }
+            }
+        }
+        val starTransition = rememberInfiniteTransition()
+        val starAlpha = starSettings.map { (duration, offset) ->
+            starTransition.animateFloat(
+                initialValue = 0.2f,
+                targetValue = 1.0f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = duration,
+                        easing = EaseInOutCubic
+                    ),
+                    repeatMode = RepeatMode.Reverse,
+                    initialStartOffset = StartOffset(offset)
+                ),
+            )
+        }
 
         LaunchedEffect(Unit) {
             var count = 0
             while (true) {
                 count++
-
                 if (count % 5 == 0) {
                     earthIndex++
-                    if (earthIndex == 3) earthIndex = 0
+                    if (earthIndex == earth.size) earthIndex = 0
+                }
+                if (count % 20 == 0) {
+                    moonIndex++
+                    if (moonIndex == moon.size) moonIndex = 0
                 }
                 if (count == 100) {
                     screen.destination?.let {
@@ -50,19 +117,43 @@ fun BoxScope.EarthScreen(
                         )
                     }
                 }
-
                 delay(100)
             }
         }
 
+        for (i in 0 ..< 20) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .offset(
+                        x = starPosition[i].first,
+                        y = starPosition[i].second
+                    )
+                    .alpha(starAlpha[i].value),
+                text = if (i % 5 != 0) "⭐️" else "🌟",
+                fontSize = starSize.sp,
+                lineHeight = starSize.lineSp,
+            )
+        }
         Text(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            text = "destination: ${screen.destination}",
-            fontSize = typography.small.sp,
-            lineHeight = typography.small.lineSp,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(
+                    x = earthSize.dp * 0.5f + this.screen.base * 0.08f,
+                    y = -(earthSize.dp * 0.5f + this.screen.base * 0.08f)
+                ),
+            text = moon[moonIndex],
+            fontSize = moonSize.sp,
+            lineHeight = moonSize.lineSp,
         )
         Text(
-            modifier = Modifier.align(Alignment.Center),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = { /* TODO */ }
+                    )
+                },
             text = earth[earthIndex],
             fontSize = earthSize.sp,
             lineHeight = earthSize.lineSp,
