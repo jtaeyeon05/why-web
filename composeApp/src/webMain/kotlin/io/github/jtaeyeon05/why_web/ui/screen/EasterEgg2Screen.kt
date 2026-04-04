0db7cbcbd2ba57abf6ba081daa93159c8d36056d
jtaeyeon05 <jtaeyeon05@gmail.com>
2026-04-04 23:50:29 +0900

    feat(web): add animated background and conditional close for easter egg

--- composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/ui/screen/EasterEgg2Screen.kt
+++ composeApp/src/webMain/kotlin/io/github/jtaeyeon05/why_web/ui/screen/EasterEgg2Screen.kt
@@ -1,5 +1,6 @@
 package io.github.jtaeyeon05.why_web.ui.screen
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -17,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
@@ -47,6 +50,7 @@ import io.github.jtaeyeon05.why_web.util.emojiGroups
 import io.github.jtaeyeon05.why_web.util.enableIframesToAutoplay
 import io.github.jtaeyeon05.why_web.util.nextGaussian
 import io.github.jtaeyeon05.why_web.util.specialString
+import kotlinx.coroutines.delay
 import kotlin.math.PI
 import kotlin.random.Random
 import kotlin.random.nextInt
@@ -58,193 +62,230 @@ fun BoxScope.EasterEgg2Screen(
     navController: NavController,
 ) {
     LocalLayoutConstraints.current.run {
-        val keyboardManager = LocalKeyboardEventManager.current
-
-        // BouncingEmoji
-        var bouncingEmojiKey by rememberSaveable { mutableStateOf(0) }
-        key(bouncingEmojiKey) {
-            val emojiGroup = remember { emojiGroups.random() }
-            repeat(50) {
-                BouncingEmoji(
-                    emoji = remember { emojiGroup.random() },
-                    initialDirection = remember {
-                        Random.nextGaussian(
-                            mean = 0.5 * PI,
-                            stdDev = 0.75,
-                            min = -0.5 * PI,
-                            max = 1.5 * PI
-                        ).toFloat()
-                    },
-                    initialMagnitude = remember {
-                        screen.base * (0.015f + 0.015f * Random.nextFloat())
-                    },
-                    accelDirection = 1.5f * PI.toFloat(),
-                    accelMagnitude = screen.base * 0.0003f,
-                )
-            }
-        }
-
-        // WebView
-        val (webWidth, webHeight) = run {
-            val webHeightMax = screen.height - 2 * box.selectionBoxHeight(2) - 2 * padding.large
-
-            var webWidth = screen.base - 2 * padding.large
-            var webHeight = webWidth * 3.0f / 4.0f
-            if (webHeight > webHeightMax) {
-                webWidth = webHeightMax * 4.0f / 3.0f
-                webHeight = webHeightMax
-            }
-
-            Pair(webWidth, webHeight)
-        }
-
-        val webState = rememberWebViewState(url = "https://www.youtube.com/embed/u2erFUojbAA?autoplay=1&controls=0")
-        val webController = rememberWebViewController()
-
-        Box(
-            modifier = Modifier
-                .width(webWidth)
-                .height(webHeight)
-                .align(Alignment.Center)
-                .border(
-                    width = inset.borderWidth,
-                    color = LocalContentColor.current,
-                )
-                .background(
-                    color = LocalContentColor.current
-                        .copy(alpha = 0.25f)
-                        .compositeOver(MaterialTheme.colorScheme.background),
-                )
-        ) {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(padding.large),
-                text = "WebView",
-                fontSize = typography.large.sp,
-                lineHeight = typography.large.lineSp,
-                fontWeight = FontWeight.Bold,
-            )
-            ComposeWebView(
-                modifier = Modifier
-                    .padding(inset.borderWidth)
-                    .fillMaxSize(),
-                state = webState,
-                controller = webController,
-                settings = WebViewSettings(
-                    javaScriptEnabled = true,
-                    domStorageEnabled = true,
-                    darkMode = DarkMode.DARK,
-                ),
-                onCreated = {
-                    enableIframesToAutoplay()
-                },
-            )
-        }
-
-        // Close
-        LaunchedEffect(Unit) {
-            keyboardManager.events.collect { webKeyEvent ->
-                if (webKeyEvent.isCancelPressed) {
-                    navController.popBackStack()
+        var backgroundColorKey by rememberSaveable { mutableStateOf(-1) }
+        val backgroundColor by animateColorAsState(
+            targetValue = when (backgroundColorKey) {
+                0 -> Color.Black
+                1 -> Color.Red
+                2 -> Color.Yellow
+                3 -> Color.Green
+                4 -> Color.Cyan
+                5 -> Color.Blue
+                6 -> Color.Magenta
+                else -> Color.Black
+            }.copy(alpha = 0.25f),
+            label = "backgroundColor"
+        )
+        LaunchedEffect(backgroundColorKey) {
+            if (backgroundColorKey != 0) {
+                while (backgroundColorKey + 1 in 1 .. 6) {
+                    delay(200)
+                    backgroundColorKey += 1
                 }
+                delay(500)
+                backgroundColorKey = 0
             }
         }
 
         Box(
             modifier = Modifier
-                .size(box.buttonHeight)
-                .align(Alignment.TopStart)
-                .background(MaterialTheme.colorScheme.background)
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onTap = {
-                            navController.popBackStack()
+                .fillMaxSize()
+                .background(backgroundColor)
+        ) {
+            val keyboardManager = LocalKeyboardEventManager.current
+
+            // BouncingEmoji
+            var bouncingEmojiKey by rememberSaveable { mutableStateOf(0) }
+            key(bouncingEmojiKey) {
+                val emojiGroup = remember { emojiGroups.random() }
+                repeat(50) {
+                    BouncingEmoji(
+                        emoji = remember { emojiGroup.random() },
+                        initialDirection = remember {
+                            Random.nextGaussian(
+                                mean = 0.5 * PI,
+                                stdDev = 0.75,
+                                min = -0.5 * PI,
+                                max = 1.5 * PI
+                            ).toFloat()
                         },
+                        initialMagnitude = remember {
+                            screen.base * (0.015f + 0.015f * Random.nextFloat())
+                        },
+                        accelDirection = 1.5f * PI.toFloat(),
+                        accelMagnitude = screen.base * 0.0003f,
                     )
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "X",
-                style = LocalTextStyle.current.copy(
-                    fontSize = typography.medium.sp,
-                    textAlign = TextAlign.Center,
-                    lineHeight = typography.medium.lineSp,
-                    lineHeightStyle = LineHeightStyle(
-                        alignment = LineHeightStyle.Alignment.Center,
-                        trim = LineHeightStyle.Trim.None
+                }
+            }
+
+            // WebView
+            val (webWidth, webHeight) = run {
+                val webHeightMax = screen.height - 2 * box.selectionBoxHeight(2) - 2 * padding.large
+
+                var webWidth = screen.base - 2 * padding.large
+                var webHeight = webWidth * 3.0f / 4.0f
+                if (webHeight > webHeightMax) {
+                    webWidth = webHeightMax * 4.0f / 3.0f
+                    webHeight = webHeightMax
+                }
+
+                Pair(webWidth, webHeight)
+            }
+
+            val webState = rememberWebViewState(url = "https://www.youtube.com/embed/u2erFUojbAA?autoplay=1&controls=0")
+            val webController = rememberWebViewController()
+
+            Box(
+                modifier = Modifier
+                    .width(webWidth)
+                    .height(webHeight)
+                    .align(Alignment.Center)
+                    .border(
+                        width = inset.borderWidth,
+                        color = LocalContentColor.current,
+                    )
+                    .background(
+                        color = LocalContentColor.current
+                            .copy(alpha = 0.25f)
+                            .compositeOver(MaterialTheme.colorScheme.background),
+                    )
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(padding.large),
+                    text = "WebView",
+                    fontSize = typography.large.sp,
+                    lineHeight = typography.large.lineSp,
+                    fontWeight = FontWeight.Bold,
+                )
+                ComposeWebView(
+                    modifier = Modifier
+                        .padding(inset.borderWidth)
+                        .fillMaxSize(),
+                    state = webState,
+                    controller = webController,
+                    settings = WebViewSettings(
+                        javaScriptEnabled = true,
+                        domStorageEnabled = true,
+                        darkMode = DarkMode.DARK,
                     ),
-                ),
-            )
-        }
-
-        // Selection
-        var selection by rememberSaveable { mutableStateOf(1) }
-        var selectionScrollTo by rememberSaveable { mutableStateOf(1.5f) }
-        LaunchedEffect(Unit) {
-            keyboardManager.events.collect { webKeyEvent ->
-                if (webKeyEvent.isUpPressed) {
-                    selection = (selection - 1).coerceIn(0 ..< 3)
-                    selectionScrollTo = selection.toFloat()
-                } else if (webKeyEvent.isDownPressed) {
-                    selection = (selection + 1).coerceIn(0 ..< 3)
-                    selectionScrollTo = selection.toFloat()
-                }
-                if (webKeyEvent.isAnyPressed(WebKey.SHIFT_LEFT, WebKey.SHIFT_RIGHT)) {
-                    bouncingEmojiKey += 1
-                }
-            }
-        }
-
-        SelectionBox(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            scrollTo = selectionScrollTo,
-            line = 2,
-        ) {
-            ClassicButton(
-                modifier = Modifier.fillMaxWidth(),
-                focused = selection == 0,
-                onClick = { /* TODO */ },
-                onFocused = { selection = 0 },
-            ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = rememberSaveable { specialString(Random.nextInt(3 .. 6)) },
+                    onCreated = {
+                        enableIframesToAutoplay()
+                    },
                 )
             }
-            ClassicButton(
-                modifier = Modifier.fillMaxWidth(),
-                focused = selection == 1,
-                onClick = {
-                    val urlList = listOf(
-                        "https://www.youtube.com/embed/eMGRt0A9Yns?autoplay=1&controls=0",
-                        "https://www.youtube.com/embed/vR21RMEV0fg?autoplay=1&controls=0",
-                        "https://www.youtube.com/embed/6zAkiKy76xs?autoplay=1&controls=0",
-                        "https://www.youtube.com/embed/5mGuCdlCcNM?autoplay=1&controls=0",
-                        "https://www.youtube.com/embed/3e6motL4QMc?autoplay=1&controls=0",
-                        "https://www.youtube.com/embed/tcHaMWktCYE?autoplay=1&controls=0",
+
+            // Close
+            val canBack by derivedStateOf { navController.previousBackStackEntry != null }
+
+            LaunchedEffect(canBack) {
+                if (canBack) {
+                    keyboardManager.events.collect { webKeyEvent ->
+                        if (webKeyEvent.isCancelPressed) {
+                            navController.popBackStack()
+                        }
+                    }
+                }
+            }
+
+            if (canBack) {
+                Box(
+                    modifier = Modifier
+                        .size(box.buttonHeight)
+                        .align(Alignment.TopStart)
+                        .background(MaterialTheme.colorScheme.background)
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = {
+                                    navController.popBackStack()
+                                },
+                            )
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "X",
+                        style = LocalTextStyle.current.copy(
+                            fontSize = typography.medium.sp,
+                            textAlign = TextAlign.Center,
+                            lineHeight = typography.medium.lineSp,
+                            lineHeightStyle = LineHeightStyle(
+                                alignment = LineHeightStyle.Alignment.Center,
+                                trim = LineHeightStyle.Trim.None
+                            ),
+                        ),
                     )
-                    webController.loadUrl(urlList.random())
-                    webController.reload()
-                },
-                onFocused = { selection = 1 },
-            ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = rememberSaveable { specialString(Random.nextInt(3 .. 6)) },
-                )
+                }
             }
-            ClassicButton(
-                modifier = Modifier.fillMaxWidth(),
-                focused = selection == 2,
-                onClick = { bouncingEmojiKey += 1 },
-                onFocused = { selection = 2 },
+
+            // Selection
+            var selection by rememberSaveable { mutableStateOf(1) }
+            var selectionScrollTo by rememberSaveable { mutableStateOf(1.5f) }
+            LaunchedEffect(Unit) {
+                keyboardManager.events.collect { webKeyEvent ->
+                    if (webKeyEvent.isUpPressed) {
+                        selection = (selection - 1).coerceIn(0..<3)
+                        selectionScrollTo = selection.toFloat()
+                    } else if (webKeyEvent.isDownPressed) {
+                        selection = (selection + 1).coerceIn(0..<3)
+                        selectionScrollTo = selection.toFloat()
+                    }
+                    if (webKeyEvent.isAnyPressed(WebKey.SHIFT_LEFT, WebKey.SHIFT_RIGHT)) {
+                        bouncingEmojiKey += 1
+                    }
+                }
+            }
+
+            SelectionBox(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                scrollTo = selectionScrollTo,
+                line = 2,
             ) {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = rememberSaveable { specialString(Random.nextInt(3 .. 6)) },
-                )
+                ClassicButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    focused = selection == 0,
+                    onClick = { backgroundColorKey = 1 },
+                    onFocused = { selection = 0 },
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = rememberSaveable { specialString(Random.nextInt(3..6)) },
+                    )
+                }
+                ClassicButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    focused = selection == 1,
+                    onClick = {
+                        val urlList = listOf(
+                            "https://www.youtube.com/embed/eMGRt0A9Yns?autoplay=1&controls=0",
+                            "https://www.youtube.com/embed/vR21RMEV0fg?autoplay=1&controls=0",
+                            "https://www.youtube.com/embed/6zAkiKy76xs?autoplay=1&controls=0",
+                            "https://www.youtube.com/embed/5mGuCdlCcNM?autoplay=1&controls=0",
+                            "https://www.youtube.com/embed/3e6motL4QMc?autoplay=1&controls=0",
+                            "https://www.youtube.com/embed/tcHaMWktCYE?autoplay=1&controls=0",
+                        )
+                        webController.loadUrl(urlList.random())
+                        webController.reload()
+                    },
+                    onFocused = { selection = 1 },
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = rememberSaveable { specialString(Random.nextInt(3..6)) },
+                    )
+                }
+                ClassicButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    focused = selection == 2,
+                    onClick = { bouncingEmojiKey += 1 },
+                    onFocused = { selection = 2 },
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = rememberSaveable { specialString(Random.nextInt(3..6)) },
+                    )
+                }
             }
         }
     }
